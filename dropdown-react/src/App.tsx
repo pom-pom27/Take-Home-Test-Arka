ab311b798af168b0e06039cf295772b38a20f78b
selja <seljaworks@gmail.com>
2024-03-24 19:26:50 +0700

    separate list data from the component

--- dropdown-react/src/App.tsx
+++ dropdown-react/src/App.tsx
@@ -1,33 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import MyDropdown from "./components/MyDropdown";
-
-const jenjangList = [
-  "Pendidikan Pra Sekolah",
-  "Pendidikan Dasar",
-  "Pendidikan Menengah",
-  "Pendidikan Tinggi",
-];
-
-const tingkatanList = [
-  [
-    "PAUD (Pendidikan Anak Usia Dini)",
-    "TK (Taman Kanak-kanak)",
-    "RA (Raudhatul Athfal)",
-  ],
-  [
-    "SD (Sekolah Dasar)",
-    "MI (Madrasah Ibtidaiyah)",
-    "SMP (Sekolah Menengah Pertama)",
-    "MTs (Madrasah Tsanawiyah)",
-  ],
-  [
-    "SMA (Sekolah Menengah Atas)",
-    "MA (Madrasah Aliyah)",
-    "SMK (Sekolah Menengah Kejuruan)",
-  ],
-  ["D3 Diploma", "S1/D4 Sarjana", "S2 Magister", "S3 Doktoral"],
-];
+import { jenjangList, tingkatanList } from "./data";
 
 function App() {
   const [isJenjangDropdownOpen, setIsJenjangDropdownOpen] = useState(false);
---
+++ dropdown-react/src/data.ts
@@ -0,0 +1,26 @@
+export const jenjangList = [
+  "Pendidikan Pra Sekolah",
+  "Pendidikan Dasar",
+  "Pendidikan Menengah",
+  "Pendidikan Tinggi",
+];
+
+export const tingkatanList = [
+  [
+    "PAUD (Pendidikan Anak Usia Dini)",
+    "TK (Taman Kanak-kanak)",
+    "RA (Raudhatul Athfal)",
+  ],
+  [
+    "SD (Sekolah Dasar)",
+    "MI (Madrasah Ibtidaiyah)",
+    "SMP (Sekolah Menengah Pertama)",
+    "MTs (Madrasah Tsanawiyah)",
+  ],
+  [
+    "SMA (Sekolah Menengah Atas)",
+    "MA (Madrasah Aliyah)",
+    "SMK (Sekolah Menengah Kejuruan)",
+  ],
+  ["D3 Diploma", "S1/D4 Sarjana", "S2 Magister", "S3 Doktoral"],
+];
